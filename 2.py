c24b0a4b9ef93993ca0fab9e2112cde81a40167c
Danila-lab-max <69604941+Danila-lab-max@users.noreply.github.com>
2020-12-23 20:40:20 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -22,6 +22,7 @@ if __name__ == '__main__':
     point1 = 0
     clock = pygame.time.Clock()
     while running:
+        ver = random.choice([1] + [0] * 100)
         if point // 100 != point1 // 100:
             print(point // 100)
         point1 = point
@@ -30,23 +31,24 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                tim = 0
-                drawing = True  # включаем режим рисования
-                # запоминаем координаты одного угла
-                x1, y1 = random.randint(80, 421), 0
-                list1.append([-1, -1])
-                list.append([x1, y1])
+            # if event.type == pygame.MOUSEBUTTONDOWN:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     cor_x += 10
                 if event.key == pygame.K_LEFT:
                     cor_x -= 10
-                if event.key == pygame.K_UP:
-                    cor_y -= 10
-                if event.key == pygame.K_DOWN:
-                    cor_y += 10
-
+                if event.key == pygame.K_w:
+                    v += 10
+                if event.key == pygame.K_s:
+                    v -= 10
+                else:
+                    break
+        if ver == 1:
+            tim = 0
+            drawing = True
+            x1, y1 = random.randint(80, 421), 0
+            list1.append([-1, -1])
+            list.append([x1, y1])
         surf1 = pygame.image.load('car_pygame.png')
         surf = pygame.transform.scale(surf1, (80, 100))
         rect = surf.get_rect(
@@ -55,26 +57,29 @@ if __name__ == '__main__':
         screen2.blit(screen, (0, 0))
         # рисуем на экране сохранённое на втором холсте
         screen.blit(screen2, (0, 0))
-        s = v * clock.tick() / 1000
-        if drawing:  # и, если надо, текущий прямоугольник
-            for i in range(len(list)):
-                surf1 = pygame.image.load('3 (2).png')
-                surf = pygame.transform.scale(surf1, (80, 100))
-                rect = surf.get_rect(
-                    bottomright=(list[i][0], list[i][1]))
-                screen.blit(surf, rect)
-                # list[i][0] += s / list1[i][0]
-                list[i][1] -= s / list1[i][1]
-                if list[i][0] == cor_x or list[i][1] == cor_y:
-                    point = 0
-            l = list.copy()
-            list = []
-            for i in l:
-                if i[1] >= 501:
-                    continue
-                else:
-                    list.append(i)
-                if -30 <= i[0] - cor_x <= 30 and -30 <= i[1] - cor_y <= 30:
-                    point = 0
+        s = v * clock.tick() / 1000  # и, если надо, текущий прямоугольник
+        for i in range(len(list)):
+            if point // 100 != point1 // 100:
+                print(point // 100)
+            point1 = point
+            point += 1
+            surf1 = pygame.image.load('3 (2).png')
+            surf = pygame.transform.scale(surf1, (80, 100))
+            rect = surf.get_rect(
+                bottomright=(list[i][0], list[i][1]))
+            screen.blit(surf, rect)
+            # list[i][0] += s / list1[i][0]
+            list[i][1] -= s / list1[i][1]
+            if list[i][0] == cor_x or list[i][1] == cor_y:
+                point = 0
+        l = list.copy()
+        list = []
+        for i in l:
+            if i[1] >= 501:
+                continue
+            else:
+                list.append(i)
+            if -30 <= i[0] - cor_x <= 30 and -30 <= i[1] - cor_y <= 30:
+                point = 0
         screen2.blit(screen, (0, 0))
         pygame.display.flip()

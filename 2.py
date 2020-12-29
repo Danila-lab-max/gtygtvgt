49cc1f5783b6a17dc32919e14c5024dd19b5cc26
Danila-lab-max <69604941+Danila-lab-max@users.noreply.github.com>
2020-12-29 11:06:08 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,8 +1,81 @@
 import time
 import pygame
 import random
+import os
+import sys
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def draw(screen, score, speed):
+    screen.fill((255, 255, 255))
+    font = pygame.font.Font(None, 30)
+    text = font.render("Score:" + score, True, (100, 255, 100))
+    screen.blit(text, (0, 0))
+    text1 = font.render("Speed:" + speed, True, (100, 255, 100))
+    screen.blit(text1, (0, 60))
+
+
+def load_image(name, color_key=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Не загрузилось:', name)
+        raise SystemExit(message)
+    image = image.convert_alpha()
+    if color_key is not None:
+        if color_key is -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    return image
+
+
+pygame.init()
+screen_size = (500, 500)
+screen = pygame.display.set_mode(screen_size)
+FPS = 50
+
+clock = pygame.time.Clock()
+
+
+def start_screen():
+    intro_text = ["Правила:",
+                  "Нужно",
+                  "передвигать машину",
+                  "клавишами,",
+                  "не врезаясь в другие машины.",
+                  "W - увелечение скорости, S - уменьшение."]
+
+    fon = pygame.transform.scale(load_image('p.jpg'), screen_size)
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 40
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+        clock.tick(FPS)
+
 
 if __name__ == '__main__':
+    start_screen()
     pygame.init()
     pygame.display.set_caption('Шарики')
     size = width, height = 501, 501
@@ -12,6 +85,7 @@ if __name__ == '__main__':
     x1, y1 = 0, 0
     drawing = False  # режим рисования выключен
     v = 100
+    v_show = 60
     list = []
     list1 = []
     y_go = -1
@@ -34,21 +108,31 @@ if __name__ == '__main__':
             # if event.type == pygame.MOUSEBUTTONDOWN:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    cor_x += 10
+                    if cor_x <= 390:
+                        cor_x += 10
                 if event.key == pygame.K_LEFT:
-                    cor_x -= 10
+                    if cor_x >= 190:
+                        cor_x -= 10
                 if event.key == pygame.K_w:
                     v += 10
+                    v_show += 1
                 if event.key == pygame.K_s:
                     v -= 10
+                    v_show -= 1
                 else:
                     break
         if ver == 1:
             tim = 0
             drawing = True
-            x1, y1 = random.randint(80, 421), 0
+            x1, y1 = random.randint(190, 390), 0
             list1.append([-1, -1])
             list.append([x1, y1])
+        draw(screen, str(point // 100), str(v_show))
+        surf1 = pygame.image.load('road.jpg')
+        surf = pygame.transform.scale(surf1, (300, 501))
+        rect = surf.get_rect(
+            bottomright=(400, 501))
+        screen.blit(surf, rect)
         surf1 = pygame.image.load('car_pygame.png')
         surf = pygame.transform.scale(surf1, (80, 100))
         rect = surf.get_rect(
@@ -83,3 +167,4 @@ if __name__ == '__main__':
                 point = 0
         screen2.blit(screen, (0, 0))
         pygame.display.flip()
+
